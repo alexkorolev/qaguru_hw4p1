6fc57db18bdc59c3cac1b1d71c939d921fb7a955
Aleksei Korolev <alexkorolev86@gmail.com>
2023-11-01 23:59:00 +0300

    commit 2

--- src/test/java/GitHubTest.java
+++ src/test/java/GitHubTest.java
@@ -8,50 +8,33 @@ import static com.codeborne.selenide.Selenide.*;
 public class GitHubTest {
     @BeforeEach
     void beforeMethod(){
-        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.baseUrl = "https://github.com/";
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
     }
 
     @Test
     void fillThePracticeForm() {
-        open("/automation-practice-form");
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
+        String codeJUnit5 =
+                "@ExtendWith({SoftAssertsExtension.class})\n" +
+                        "class Tests {\n" +
+                        "  @Test\n" +
+                        "  void test() {\n" +
+                        "    Configuration.assertionMode = SOFT;\n" +
+                        "    open(\"page.html\");\n" +
+                        "\n" +
+                        "    $(\"#first\").should(visible).click();\n" +
+                        "    $(\"#second\").should(visible).click();\n" +
+                        "  }\n" +
+                        "}";
 
-        $("#firstName").setValue("FirstName");
-        $("#lastName").setValue("LastName");
-        $("#userEmail").setValue("test@test.com");
-        $("#gender-radio-1").doubleClick();
-        $("#userNumber").setValue("9990002233");
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".wiki-rightbar ul li button").click();
+        $(".wiki-rightbar ul").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text(codeJUnit5));
 
-        $("#dateOfBirthInput").click();
-        $("select[class*='month-select']").selectOptionByValue("9");
-        $("select[class*='year-select']").selectOptionByValue("1985");
-        $("div[aria-label*='October 30th']").click();
-
-        $("#subjectsContainer input").click();
-        $("#subjectsContainer input").setValue("English").pressEnter();
-
-        $("#hobbiesWrapper").$(byText("Reading")).click();
-
-        $("#uploadPicture").uploadFromClasspath("my.png");
-
-        $("#currentAddress").setValue("Lenina street 100 , flat 101");
-
-        $("#state input").val("NCR").pressEnter();
-        $("#city input").val("Noida").pressEnter();
-        $("#submit").click();
-
-        $(".table-responsive").shouldHave(text("FirstName LastName"));
-        $(".table-responsive").shouldHave(text("test@test.com"));
-        $(".table-responsive").shouldHave(text("Male"));
-        $(".table-responsive").shouldHave(text("9990002233"));
-        $(".table-responsive").shouldHave(text("30 October,1985"));
-        $(".table-responsive").shouldHave(text("English"));
-        $(".table-responsive").shouldHave(text("Reading"));
-        $(".table-responsive").shouldHave(text("my.png"));
-        $(".table-responsive").shouldHave(text("Lenina street 100 , flat 101"));
-        $(".table-responsive").shouldHave(text("NCR Noida"));
-    }
+        sleep(3000);
+      }
 }
